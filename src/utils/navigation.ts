a306c878c5b1f04e96eb35a177c8802908f48348
auliakbarh <auliaakbarharahaap@gmail.com>
2024-05-20 04:34:52 +0700

    :beers: update website content

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -15,20 +15,21 @@ const footerLinks = [
   {
     section: "Yuk Investasi Saham",
     links: [
-      { name: "About", url: "#" },
-      { name: "Open Account", url: "#" },
+      { name: "About", url: "/about" },
+      { name: "Open Account", url: "/services" },
       { name: "Articles", url: "/blog" },
       { name: "Contact", url: "/contact" },
     ],
   },
 ];
 // An object of links for social icons
-const socialLinks = {
+export const socialLinks = {
   facebook: "https://www.facebook.com/",
   x: "https://twitter.com/",
   telegram: "http://t.me/yukinvestasisahamid",
   instagram: "https://www.instagram.com/stockenthusias.id/",
-  whatsapp: "https://wa.me/message/ZR63IOXREFFOD1"
+  whatsapp: "https://wa.me/message/ZR63IOXREFFOD1",
+  openAccount: "https://login.miraeasset.co.id/registration/oe?3102663="
 };
 
 export default {
